554a8a23ea14013cb3048cab04130de4062ea56a
michaelvanderlinden <michael.vanderlinden@yale.edu>
2016-04-28 16:14:10 +0000

    foo and gif urls and views

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,5 +11,7 @@ import hello.views
 
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
+    url(r'^foo/', hello.views.foo, name='foo'),
+    url(r'^mrw/semester-ends.gif', name='gif'),
     url(r'^admin/', include(admin.site.urls)),
 ]
